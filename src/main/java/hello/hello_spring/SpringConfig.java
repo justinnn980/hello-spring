135f9741509c16d0a810648cceec1545c1bd1269
justinnn980 <justinnn980@gmail.com>
2025-09-26 18:00:54 +0900

    Update: Render 해보기 3치 수정 h2 삭제

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,23 +1,13 @@
 package hello.hello_spring;
 
-import hello.hello_spring.repository.JdbcNumberRepository;
 import hello.hello_spring.repository.MemoryNumberRepository;
 import hello.hello_spring.repository.NumberRepository;
 import hello.hello_spring.service.NumberService;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-
-    private DataSource dataSource;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
     @Bean
     public NumberService numberService() {
@@ -26,7 +16,6 @@ public class SpringConfig {
 
     @Bean
     public NumberRepository numberRepository() {
-//        return new MemoryNumberRepository();
-        return new JdbcNumberRepository(dataSource);
+        return new MemoryNumberRepository();
     }
 }
